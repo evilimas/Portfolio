8cd10b1ed5356f6c004ad196e8dcd439069ac13b
evilimas <eg.vilimas@gmail.com>
2023-10-06 15:36:01 +0200

    first

---
+++ src/components/Footer.svelte
@@ -0,0 +1,34 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b> eg.vilimas@gmail.com</p>
+        <p><b class="pr-2">GitHub</b>
+            <a class="text-violet-400" target="_blank" href="https://github.com/evilimas">
+                evilimas<sup><span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                </span></sup>
+            </a>
+        </p>
+        <p><b class="pr-2">LinkedIn</b>
+            <a class="text-violet-400" target="_blank" href="https://www.linkedin.com/in/front-end-developer-egidijus-vilimas/">
+                Egidijus V.<sup><span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                </span></sup>
+            </a>
+        </p>
+        <a  
+        href="EgidijusCV.pdf"
+        target="_blank"
+        class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"/>
+            
+        
+        <h4 class="relative z-9">CV</h4>
+     </a>
+        
+    </div>
+    
+
+</footer>
