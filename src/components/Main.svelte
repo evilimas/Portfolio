05d66119b6e95573b45f6907ca49f9a8583c2805
evilimas <eg.vilimas@gmail.com>
2023-11-21 16:29:02 +0100

    fix

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -23,7 +23,7 @@
     <section id="introPage" class="grid grid-col-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi! I'm <span class="poppins text-violet-400">Egidijus</span> Vilimas <br> Front End 
+                Hi! I'm <span class="poppins text-violet-400">Egidijus</span> Vilimas<br> Front-End 
                 <span class="poppins text-violet-400">Developer</span>
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
