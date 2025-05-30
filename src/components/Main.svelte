0f71d8d1f0e966482a28e449f3b9cebb0fe294e9
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-05-30 14:51:50 +0200

    small update

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -12,7 +12,7 @@
     ]
 
     let benefits = [
-        {name: 'A self taught developer', description: 'I started with a short beginners bootcamp and then i taught myself to code using free and paid online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks and much much more.'},
+        {name: 'A self taught developer', description: 'I started with a short beginners bootcamp and then i taught myself to code using free and paid online resources and absolutely fell in love with the creativity and problem solving. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, Typescript a little of C# and much much more.'},
 
         {name: 'An excellent communicator and a team player', description: "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team."},
 
@@ -28,7 +28,7 @@
                 <span class="poppins text-violet-400">Developer</span>
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
-                My <span class="text-violet-400">favorite tech</span> includes: JavaScript, Vue, React, Sass, Tailwind & Firebase
+                My <span class="text-violet-400">favorite tech</span> includes: TypeScript, Vue, React, Sass, Tailwind and Svelte
             </p>
             <a href="https://www.linkedin.com/in/front-end-developer-egidijus-vilimas/" 
             target="_blank" 
@@ -39,7 +39,7 @@
             </a>
         </div>
         <div class="relative shadow-2xl grid place-items-center">
-            <img src={"images/profile5.png"} alt="Profile image" class="object-cover z-[2] max-h-[70vh] "/>
+            <img src={"images/profile5.png"} alt="Profile" class="object-cover z-[2] max-h-[70vh] "/>
         </div>
     </section>
 
