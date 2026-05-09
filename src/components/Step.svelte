1e016327bb02556af77982f987708e0a6f3244d1
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-05-09 19:21:42 +0200

    x

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -7,7 +7,6 @@
     target="_blank" 
     class="p-4 sm:p-6 md:p-8 flex flex-col gap-4 rounded-lg border border-solid border-violet-700 text-center group cursor-pointer hover:border-violet-400 duration-200"
     >
-
     <div 
         class="bg-slate-950 grid place-items-center px-4 text-5xl md:text-6xl -mt-10 sm:-mt-12 md:-mt-14 lg:-mt-16 mx-auto duration-200">
 
@@ -32,5 +31,6 @@
                 <p class="relative z-4">Github &rarr;</p>
             </a>
         {/if}
+
     </div>
 </a>
