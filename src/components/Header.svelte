2d912b16df0afe75adf994a0acb01db8e37d7947
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-06-11 10:48:21 +0200

    x

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -14,7 +14,7 @@
     y > 0 ? 'py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent'
 )}">
     <h1 class="font-medium">
-        <b class="font-bold poppins">Egidijus</b>Vilimas
+        <b class="font-bold poppins">Egidijus</b> Vilimas
     </h1>
     <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
         {#each tabs as tab, index }
